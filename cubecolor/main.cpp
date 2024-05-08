c96516bdffc9e4d1fcdb63b5f9c8661911db4887
mucki <code@mucki.dev>
2024-05-08 16:11:07 -0700

    new project to color n dimensional hypercube with n colors so that color sets have minimum hamming distance.

---
+++ cubecolor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cubecolor
+//
+//  Created by Michael Lacher on 5/8/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
